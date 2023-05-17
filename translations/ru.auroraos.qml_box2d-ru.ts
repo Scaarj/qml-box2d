0f1dca4576d445482950db43999224b47be132a6
Дубров Степан <s.dubrov@iva.ru>
2023-05-17 11:34:57 +0700

    fix

--- translations/ru.auroraos.qml_box2d-ru.ts
+++ translations/ru.auroraos.qml_box2d-ru.ts
@@ -2,25 +2,6 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name>AboutPage</name>
-    <message>
-        <source>About Application</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>#descriptionText</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>3-Clause BSD License</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>#licenseText</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
     <name>DefaultCoverPage</name>
     <message>
         <source>Template</source>
